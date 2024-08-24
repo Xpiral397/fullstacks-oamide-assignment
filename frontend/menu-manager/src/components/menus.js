fb8f752ea7c301f5f780693033a5a51bd4a8ede6
Xpiral397 <111965825+Xpiral397@users.noreply.github.com>
2024-08-24 10:03:22 +0100

    add frontend backend  deploy

--- frontend/menu-manager/src/components/menus.js
+++ frontend/menu-manager/src/components/menus.js
@@ -98,18 +98,15 @@ const MenuManager=() => {
           </option>
         ))}
 
-        <div className="mt-3">
-          <select
-            className="w-full p-3 border-none focus:outline-none bg-slate-50"
-            onChange={handleActionChange}
-          > <option>-----Select Action------</option>
-            <option value="add-new-root">New Menu</option>
-            <option value="add-new-node-number">Child To Menu </option>
-            <option value="add-new-child">Add Child</option>
-            <option value="delete-node">Delete Node</option>
-            <option value="delete-node">Delete Item</option>
-          </select>
-        </div>
+
+        <option>-----Select Action------</option>
+        <option value="add-new-root">New Menu</option>
+        <option value="add-new-node-number">Child To Menu </option>
+        <option value="add-new-child">Add Child</option>
+        <option value="delete-node">Delete Node</option>
+        <option value="delete-node">Delete Item</option>
+      </select>
+
 
 
     </div>
