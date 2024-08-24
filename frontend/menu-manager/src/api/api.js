6d486fb16b3266f1f93bb5422bafb80465bf25bc
Xpiral397 <111965825+Xpiral397@users.noreply.github.com>
2024-08-24 09:48:04 +0100

    add frontend backend  deploy

--- frontend/menu-manager/src/api/api.js
+++ frontend/menu-manager/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance=axios.create({
-    baseURL: 'https://ouivouchers.pythonanywhere.com/', // Adjust the base URL as needed
+    baseURL: 'https://ouivouchers.pythonanywhere.com/api/', // Adjust the base URL as needed
     headers: {
         'Content-Type': 'application/json',
     },
